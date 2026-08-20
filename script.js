a757825d9b8b99bdb5b8a4901e7a8db1320a963c
jeffersonjunior18-hub <jefferson.junior18@escola.pr.gov.br>
2026-08-20 18:31:40 +0000

    2

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function mostrarMensagem() {
     alert("Bem-vindo ao Empreenda+! 🚀");
 }
+
+function lerPost(numero) {
+
+    if (numero === 1) {
+        alert("5 passos para começar a empreender: tenha uma ideia, conheça seu público, faça um planejamento, divulgue seu negócio e continue aprendendo!");
+    }
+
+    if (numero === 2) {
+        alert("Boas ideias podem surgir observando problemas do dia a dia e pensando em maneiras de solucioná-los.");
+    }
+
+    if (numero === 3) {
+        alert("Uma startup é uma empresa criada para desenvolver um modelo de negócio inovador e com potencial de crescimento.");
+    }
+
+}
